5811e628b68ffbb1be2fe829c205759a86156dfe
soomvalle <60403747+soomvalle@users.noreply.github.com>
2020-04-03 20:19:32 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
+import Footer from "./Footer";
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <footer>
+        This project{" "}
+        <a
+          href="https://github.com/soomvalle/weather-app-react-final"
+          target="blank"
+        >
+          is open-sourced.
+        </a>
+      </footer>
+    </div>
+  );
+}
